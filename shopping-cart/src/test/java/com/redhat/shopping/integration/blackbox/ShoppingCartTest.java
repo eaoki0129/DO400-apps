15bb31ac62e2eb5a4de938719a4845718c1d9652
EAOKI <aoki.eisuke@nttcom.co.jp>
2021-11-03 22:58:05 -0400

    Refactor

--- shopping-cart/src/test/java/com/redhat/shopping/integration/blackbox/ShoppingCartTest.java
+++ shopping-cart/src/test/java/com/redhat/shopping/integration/blackbox/ShoppingCartTest.java
@@ -8,16 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.Test;
 
-import java.beans.Transient;
 import java.util.Random;
 
 import static io.restassured.RestAssured.delete;
 
 import static io.restassured.RestAssured.given;
 
-@QuarkusTest
-
-public class ShoppingCartTest {
+@QuarkusTestpublic class ShoppingCartTest {
 
     private int randomQuantity() {
 
@@ -27,105 +24,7 @@ public class ShoppingCartTest {
 
     private void addProductToTheCartWithIdAndRandomQuantity(int productId) {
 
-        AddToCartCommand productToAdd = new AddToCartCommand(
-
-            productId,
-
-            this.randomQuantity()
-
-        );
-
-        given()
-
-        .contentType("application/json")
-
-        .body(productToAdd)
-
-        .put("/cart");
-
+        AddToCartCommand productToAdd
     }
-
-    @BeforeEach
-    public void clearCart() {
-
-        delete("/cart");
-
     }
-
-   @Test
-
-   public void removingNonExistingProductInCatalogReturns400() {
-
-    given()
-
-    .pathParam("id", 9999)
-
-    .when()
-
-    .delete("/cart/products/{id}")
-
-    .then()
-
-    .statusCode(400);
-
-   
-   }
-@Test
-public void removingNonAddedProductToTheCartReturns404() {
-
-    given()
-
-    .pathParam("id", 1)
-
-    .when()
-
-    .delete("/cart/products/{id}")
-
-    .then()
-
-    .statusCode(404);
-
 }
-@Test
-public void removingTheOnlyProductInCartReturns204() {
-
-    this.addProductToTheCartWithIdAndRandomQuantity(1);
-        given()
-
-        .pathParam("id", 1)
-
-        .when()
-
-        .delete("/cart/products/{id}")
-
-        .then()
-
-        .statusCode(204);
-
-}
-@Test
-public void
-
-removingProductFromCartContainingMultipleAndDifferentProductsReturns200() {
-
-    this.addProductToTheCartWithIdAndRandomQuantity(1);
-    this.addProductToTheCartWithIdAndRandomQuantity(2);
-        given()
-
-            .pathParam("id", 1)
-
-            .when()
-
-            .delete("/cart/products/{id}")
-
-            .then()
-
-            .statusCode(200);
-
-}
-}
-
-
-
-
-
